823e54a5f672262fc5b6ac46d96e75b4c4c931b8
9Rubi <1344760467@qq.com>
2019-06-17 20:50:11 +0800

    .

--- src/java/ink/rubi/calc/controller/MainController.java
+++ src/java/ink/rubi/calc/controller/MainController.java
@@ -2,30 +2,31 @@ package ink.rubi.calc.controller;
 
 import ink.rubi.calc.controller.card.ModuleController;
 import ink.rubi.calc.controller.card.ShieldController;
-import ink.rubi.calc.controller.coventer.IntegerConverter;
+import ink.rubi.calc.controller.constant.ModuleFactory;
+import ink.rubi.calc.controller.constant.Reality;
+import ink.rubi.calc.controller.constant.ShieldFactory;
 import ink.rubi.calc.po.ModuleProperty;
 import ink.rubi.calc.po.ShieldProperty;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.IntegerBinding;
-import javafx.beans.binding.NumberBinding;
-import javafx.beans.binding.StringBinding;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.util.StringConverter;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 /**
  * @author Henry Wang
  * @since 2019-06-17 12:46
  */
 public class MainController implements Initializable {
+
+    public Button calc;
     @FXML
     private Label capacityVal, speedVal, delayVal, maxHpDecreaseVal, maxHpIncrease, recovery;
     @FXML
@@ -40,7 +41,7 @@ public class MainController implements Initializable {
     @SuppressWarnings("unchecked")
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ModuleProperty module = moduleCardController.getModule();
+    /*    ModuleProperty module = moduleCardController.getModule();
         ShieldProperty shield = shieldCardController.getShield();
         IntegerConverter converter = new IntegerConverter();
 
@@ -48,7 +49,95 @@ public class MainController implements Initializable {
 
         Bindings.bindBidirectional(shield.levelProperty(), shieldLevel,converter);
         Bindings.bindBidirectional(module.levelProperty(), moduleLevel,converter);
-        capacityVal.textProperty().bind(shieldLevel.add(moduleLevel).asString());
+        capacityVal.textProperty().bind(shieldLevel.add(moduleLevel).asString());*/
+    }
+
+    public void doCalc(ActionEvent actionEvent) {
+        BigDecimal staticVar = new BigDecimal(10000);
+        BigDecimal perLevel = new BigDecimal(1.13);
+        BigDecimal speedPower = new BigDecimal(10.24);
+        BigDecimal delayPower = new BigDecimal(3.5);
+        BigDecimal maxHpDecreasePower = new BigDecimal(32);
+        BigDecimal capacityPower = new BigDecimal(64);
+
+
+        BigDecimal maxHpIncreasePower = new BigDecimal(16);
+        BigDecimal recoveryPower = new BigDecimal(0.5);
+
+
+        ShieldProperty shield = shieldCardController.getShield();
+        ShieldFactory shieldAlpha = shield.getAlpha().selectedItemProperty().get();
+        ShieldFactory shieldBeta = shield.getBeta().selectedItemProperty().get();
+        ShieldFactory shieldGamma = shield.getGamma().selectedItemProperty().get();
+        Reality reality = shield.getReality().selectedItemProperty().get();
+        String shieldLevel = shield.getLevel();
+
+
+        ModuleProperty module = moduleCardController.getModule();
+        ModuleFactory.BetaFactory moduleBeta = module.getBeta().selectedItemProperty().get();
+        ModuleFactory.GammaFactory moduleGamma = module.getGamma().selectedItemProperty().get();
+        String moduleLevel = module.getLevel();
+
+
+        int rateMaxHp = shieldAlpha.getMaxHpDecrease() + shieldBeta.getMaxHpDecrease() + shieldGamma.getMaxHpDecrease() + reality.getMaxHpDecrease();
+        BigDecimal maxHpDecreaseResult = new BigDecimal(rateMaxHp)
+                .divide(staticVar).add(BigDecimal.ONE)
+                .multiply(maxHpDecreasePower)
+                .multiply(perLevel.pow(Integer.valueOf(shieldLevel)))
+                .setScale(0, RoundingMode.HALF_UP);
+
+        int rateCapacity = shieldAlpha.getCapacity() + shieldBeta.getCapacity() + shieldGamma.getCapacity() + reality.getCapacity() + 7500;
+        BigDecimal capacityResult = new BigDecimal(rateCapacity)
+                .divide(staticVar).add(BigDecimal.ONE)
+                .multiply(capacityPower)
+                .multiply(perLevel.pow(Integer.valueOf(shieldLevel)))
+                .setScale(0, RoundingMode.HALF_UP);
+
+        int rateSpeed = shieldAlpha.getSpeed() + shieldBeta.getSpeed() + shieldGamma.getSpeed() + reality.getSpeed();
+        BigDecimal speedResult = new BigDecimal(rateSpeed)
+                .divide(staticVar);
+
+        if (speedResult.compareTo(BigDecimal.ZERO) >= 0) {
+            speedResult = speedPower
+                    .multiply(perLevel.pow(Integer.valueOf(shieldLevel)))
+                    .multiply(speedResult.add(BigDecimal.ONE))
+                    .setScale(0, RoundingMode.HALF_UP);
+        } else {
+            speedResult = speedPower
+                    .multiply(perLevel.pow(Integer.valueOf(shieldLevel)))
+                    .divide(speedResult.abs().add(BigDecimal.ONE), 0, RoundingMode.HALF_UP);
+        }
+
+        int rateDelay = shieldAlpha.getDelay() + shieldBeta.getDelay() + shieldGamma.getDelay();
+
+        BigDecimal delayResult = new BigDecimal(rateDelay)
+                .divide(staticVar);
+        if (delayResult.compareTo(BigDecimal.ZERO) >= 0) {
+            delayResult = delayPower.multiply(delayResult.add(BigDecimal.ONE)).setScale(2, RoundingMode.HALF_UP);
+        } else {
+            delayResult = delayPower.divide(
+                    delayResult.abs().add(BigDecimal.ONE), 2, RoundingMode.HALF_UP
+            );
+        }
+        System.out.println(capacityResult);
+        System.out.println(speedResult);
+        System.out.println(delayResult);
+        System.out.println(maxHpDecreaseResult);
+
+        BigDecimal maxHpIncreaseResult = new BigDecimal(moduleBeta.getMaxHpIncrease()).divide(staticVar).add(BigDecimal.ONE)
+                .multiply(maxHpIncreasePower)
+                .multiply(perLevel.pow(Integer.valueOf(moduleLevel)))
+                .setScale(0, RoundingMode.HALF_UP);
+        BigDecimal recoveryResult = new BigDecimal(moduleGamma.getRecovery()).divide(staticVar).add(BigDecimal.ONE)
+                .multiply(recoveryPower)
+                .multiply(perLevel.pow(Integer.valueOf(moduleLevel)))
+                .setScale(0, RoundingMode.HALF_UP);
+
+
+        System.out.println(maxHpIncreaseResult);
+        System.out.println(recoveryResult);
+        int result = (530564 - maxHpDecreaseResult.intValue()) * 1 + maxHpIncreaseResult.intValue();
+        System.out.println(result);
     }
 
 
--- src/java/ink/rubi/calc/controller/constant/ModuleFactory.java
+++ src/java/ink/rubi/calc/controller/constant/ModuleFactory.java
@@ -11,12 +11,12 @@ public class ModuleFactory {
     @Getter
     public enum BetaFactory {
         NONE("None", 0),
-        A0_B0_C0("A0_B0_C0", -1200),
-        A1_B0_C0("A1_B0_C0", -1),
-        A2_B0_C0("A2_B0_C0", -12),
-        A3_B0_C0("A3_B0_C0", -12),
-        A4_B0_C0("A4_B0_C0", -12),
-        A5_B0_C0("A5_B0_C0", 24);
+        A0_B0_C0("A0_B0_C0", 0),
+        A1_B0_C0("A1_B0_C0", 5),
+        A2_B0_C0("A2_B0_C0", 10),
+        A3_B0_C0("A3_B0_C0", 15),
+        A4_B0_C0("A4_B0_C0", 20),
+        A5_B0_C0("A5_B0_C0", 25);
         String name;
         /**
          * 最大生命值增幅
@@ -33,12 +33,12 @@ public class ModuleFactory {
     @Getter
     public enum GammaFactory {
         NONE("None", 0),
-        A0_B0_C0("A0_B0_C0", -12),
-        A0_B1_C0("A0_B1_C0", -12),
-        A0_B2_C0("A0_B2_C0", -12),
-        A0_B3_C0("A0_B3_C0", -12),
-        A0_B4_C0("A0_B4_C0", -12),
-        A0_B5_C0("A0_B5_C0", 24);
+        A0_B0_C0("A0_B0_C0", 0),
+        A0_B1_C0("A0_B1_C0", 1),
+        A0_B2_C0("A0_B2_C0", 2),
+        A0_B3_C0("A0_B3_C0", 3),
+        A0_B4_C0("A0_B4_C0", 4),
+        A0_B5_C0("A0_B5_C0", 5);
         String name;
         /**
          * 恢复速度增幅
