d7fdb41d194ad6de45945416ccc03b6ef38471b1
e <1344760467@qq.com>
2019-06-19 17:01:13 +0800

    .

--- src/main/java/ink/rubi/calc/controller/calc/Calculator.java
+++ src/main/java/ink/rubi/calc/controller/calc/Calculator.java
@@ -12,8 +12,8 @@ import java.math.RoundingMode;
  * @since 2019-06-18 11:47
  */
 public class Calculator {
-
-    private static final BigDecimal STATIC_VAR = BigDecimal.valueOf(10000L);
+    private static final BigDecimal HUNDRED = BigDecimal.valueOf(100L);
+    private static final BigDecimal TEN_THOUSAND = BigDecimal.valueOf(10000L);
     private static final BigDecimal PER_LEVEL = BigDecimal.valueOf(1.13);
     private static final BigDecimal SPEED_POWER = BigDecimal.valueOf(10.24);
     private static final BigDecimal DELAY_POWER = BigDecimal.valueOf(3.5);
@@ -29,7 +29,7 @@ public class Calculator {
 
     public static BigDecimal calcMaxHpDecrease(final int rateMaxHp, final int shieldLevel) {
         return new BigDecimal(rateMaxHp)
-                .divide(STATIC_VAR).add(BigDecimal.ONE)
+                .divide(TEN_THOUSAND).add(BigDecimal.ONE)
                 .multiply(MAX_HP_DECREASE_POWER)
                 .multiply(PER_LEVEL.pow(shieldLevel))
                 .setScale(0, RoundingMode.HALF_UP);
@@ -39,7 +39,7 @@ public class Calculator {
 
     public static BigDecimal calcCapacity(final int rateCapacity, final int shieldLevel) {
         return new BigDecimal(rateCapacity)
-                .divide(STATIC_VAR).add(BigDecimal.ONE)
+                .divide(TEN_THOUSAND).add(BigDecimal.ONE)
                 .multiply(CAPACITY_POWER)
                 .multiply(PER_LEVEL.pow(shieldLevel))
                 .setScale(0, RoundingMode.HALF_UP);
@@ -47,7 +47,7 @@ public class Calculator {
 
     public static BigDecimal calcSpeed(final int rateSpeed, final int shieldLevel) {
         BigDecimal speedResult = new BigDecimal(rateSpeed)
-                .divide(STATIC_VAR);
+                .divide(TEN_THOUSAND);
 
         if (speedResult.compareTo(BigDecimal.ZERO) >= 0) {
             speedResult = SPEED_POWER
@@ -65,7 +65,7 @@ public class Calculator {
 
     public static BigDecimal calcDelay(final int rateDelay) {
         BigDecimal delayResult = new BigDecimal(rateDelay)
-                .divide(STATIC_VAR);
+                .divide(TEN_THOUSAND);
         if (delayResult.compareTo(BigDecimal.ZERO) >= 0) {
             delayResult = DELAY_POWER.multiply(delayResult.add(BigDecimal.ONE)).setScale(2, RoundingMode.HALF_UP);
         } else {
@@ -77,14 +77,14 @@ public class Calculator {
     }
 
     public static BigDecimal calcMaxHpIncrease(final int rateMaxHpIncrease, final int moduleLevel) {
-        return new BigDecimal(rateMaxHpIncrease).divide(STATIC_VAR).add(BigDecimal.ONE)
+        return new BigDecimal(rateMaxHpIncrease).divide(HUNDRED).add(BigDecimal.ONE)
                 .multiply(MAX_HP_INCREASE_POWER)
                 .multiply(PER_LEVEL.pow(moduleLevel))
                 .setScale(0, RoundingMode.HALF_UP);
     }
 
     public static BigDecimal calcRecovery(final int rateRecovery, final int moduleLevel) {
-        return new BigDecimal(rateRecovery).divide(STATIC_VAR).add(BigDecimal.ONE)
+        return new BigDecimal(rateRecovery).divide(HUNDRED).add(BigDecimal.ONE)
                 .multiply(RECOVERY_POWER)
                 .multiply(PER_LEVEL.pow(moduleLevel))
                 .setScale(0, RoundingMode.HALF_UP);
